8ddda2889e401ce091d8683ad4674379a11f4a1e
developer.umaria <developer.umaria2021@gmail.com>
2023-06-01 10:38:53 +0530

    run ios

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/nikhilkumar/FlutterDev/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/nikhilkumar/Desktop/Affle_Projects/excelr-mobileapp-flutter"
+export "FLUTTER_ROOT=/Users/test/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/test/Documents/Affle/excelr-mobileapp-flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
